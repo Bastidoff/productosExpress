96c83284f7993df78d9fc957565a94fedf22622a
jaimezzapata <zapataval2304@gmail.com>
2022-10-30 15:13:18 -0600

    Secrea vista para mostrar mensaje de creacion de nuev usuario

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,68 +1,88 @@
-import { Usuario } from "../models/UserModel.js"
-import {check, validationResult} from 'express-validator'
+import { Usuario } from "../models/UserModel.js";
+import { check, validationResult } from "express-validator";
 
-const formularioLogin = (req, res)=>{
-  res.render('auth/login', {
-    nombreVista: 'Iniciar Sesion'
-  })
-}
-const formularioRegistro = (req, res)=>{
-  res.render('auth/registro', {
-    nombreVista: 'Nuevo Usuario'
-  })
-}
+const generarId = () =>
+  Math.random().toString(32).substring(2) + Date.now().toString(32);
+
+const formularioLogin = (req, res) => {
+  res.render("auth/login", {
+    nombreVista: "Iniciar Sesion",
+  });
+};
+const formularioRegistro = (req, res) => {
+  res.render("auth/registro", {
+    nombreVista: "Nuevo Usuario",
+  });
+};
 
 const crearUsuario = async (req, res) => {
-  await check('nombre').notEmpty().withMessage('El campo nombre es obligatorio').run(req)
-  await check('correo').isEmail().withMessage('El correo debe llevar un formato valido').run(req)
-  await check('contrasena').isLength({min:5}).withMessage('La contrasena debe tener minimo 5 caracteres').run(req)
-  await check('rcontrasena').equals('contrasena').withMessage('Las contrasenas no coinciden').run(req)
-  
-  let listadoErrores = validationResult(req)
+  await check("nombre")
+    .notEmpty()
+    .withMessage("El campo nombre es obligatorio")
+    .run(req);
+  await check("correo")
+    .isEmail()
+    .withMessage("El correo debe llevar un formato valido")
+    .run(req);
+  await check("contrasena")
+    .isLength({ min: 5 })
+    .withMessage("La contrasena debe tener minimo 5 caracteres")
+    .run(req);
+  await check("rcontrasena")
+    .equals("contrasena")
+    .withMessage("Las contrasenas no coinciden")
+    .run(req);
 
-  if(!listadoErrores.isEmpty()){
-    return res.render('auth/registro', {
-      nombreVista: 'Nuevo Usuario',
+  let listadoErrores = validationResult(req);
+
+  if (!listadoErrores.isEmpty()) {
+    return res.render("auth/registro", {
+      nombreVista: "Nuevo Usuario",
       errores: listadoErrores.array(),
       usuario: {
         nombre: req.body.nombre,
         correo: req.body.correo,
-      }
-    })
+      },
+    });
   }
-  const {nombre, correo, contrasena} = req.body
+  const { nombre, correo, contrasena } = req.body;
 
-  const validarUsuario = await Usuario.findOne({where: {correo}})
+  const validarUsuario = await Usuario.findOne({ where: { correo } });
 
-  if(validarUsuario){
-    return res.render('auth/registro', {
-      nombreVista: 'Nuevo Usuario',
-      errores:[{msg: 'El correo ya existe en la base de datos'}],
+  if (validarUsuario) {
+    return res.render("auth/registro", {
+      nombreVista: "Nuevo Usuario",
+      errores: [{ msg: "El correo ya existe en la base de datos" }],
       usuario: {
         nombre: req.body.nombre,
         correo: req.body.correo,
-      }
-    })
+      },
+    });
   }
-
 
   await Usuario.create({
     nombre,
-    correo, 
+    correo,
     contrasena,
-    token: 1234
-  })
-}
+    token: generarId(),
+  });
 
-const formularioRecuperar = (req, res)=>{
-  res.render('auth/recuperar', {
-    nombreVista: 'Recuperar Usuario'
-  })
-}
+  res.render("templates/usuarioCreado", {
+    nombreVista: "Confirmacion Usuario",
+    mensaje:
+      "Revisa tu correo electronico para confirmar la creacion de usuario",
+  });
+};
+
+const formularioRecuperar = (req, res) => {
+  res.render("auth/recuperar", {
+    nombreVista: "Recuperar Usuario",
+  });
+};
 
 export {
   formularioLogin,
   formularioRegistro,
   formularioRecuperar,
-  crearUsuario
-}
+  crearUsuario,
+};
