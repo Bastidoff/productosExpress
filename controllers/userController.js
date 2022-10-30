518ae820a59a6a9719ec8825c243923c20963f12
jaimezzapata <zapataval2304@gmail.com>
2022-10-30 14:30:48 -0600

    Se agregan las validacion de los campos indepndientes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,10 @@
 import { Usuario } from "../models/UserModel.js"
+import {check, validationResult} from 'express-validator'
 
 const formularioLogin = (req, res)=>{
-  res.render('auth/login')
+  res.render('auth/login', {
+    nombreVista: 'Iniciar Sesion'
+  })
 }
 const formularioRegistro = (req, res)=>{
   res.render('auth/registro', {
@@ -10,12 +13,34 @@ const formularioRegistro = (req, res)=>{
 }
 
 const crearUsuario = async (req, res) => {
+  await check('nombre').notEmpty().withMessage('El campo nombre es obligatorio').run(req)
+  await check('correo').isEmail().withMessage('El correo debe llevar un formato valido').run(req)
+  await check('contrasena').isLength({min:5}).withMessage('La contrasena debe tener minimo 5 caracteres').run(req)
+  await check('rcontrasena').equals('contrasena').withMessage('Las contrasenas no coinciden').run(req)
+  
+  let listadoErrores = validationResult(req)
+
+  if(!listadoErrores.isEmpty()){
+    return res.render('auth/registro', {
+      nombreVista: 'Nuevo Usuario',
+      errores: listadoErrores.array(),
+      usuario: {
+        nombre: req.body.nombre,
+        correo: req.body.correo,
+      }
+    })
+  }
+
+  res.json(listadoErrores.array())
+
   const usuario = await Usuario.create(req.body)
   res.json(usuario)
 }
 
 const formularioRecuperar = (req, res)=>{
-  res.render('auth/recuperar')
+  res.render('auth/recuperar', {
+    nombreVista: 'Recuperar Usuario'
+  })
 }
 
 export {
